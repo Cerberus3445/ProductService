8d3b7d57c6a862522cd0d3400320ed2c7e9995e4
egor <egormalov.2007@gmail.com>
2024-11-22 19:55:38 +1000

    Initial commit

---
+++ src/main/java/com/cerberus/productservice/HardwareShopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cerberus.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareShopProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareShopProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cerberus/productservice/HardwareShopProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerberus.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareShopProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
